df0812e56e5f18d61edf98858a4666bb85f64cb6
rjplaton <rjplaton@gmail.com>
2019-07-02 09:37:09 -0700

    page generator using jinja templating works and creates html pages from image jpgs

--- build.py
+++ build.py
@@ -1,15 +1,13 @@
 pages = []
 
-
 #discover all the jpg files in the /images/ directory and save as a list
 import glob
 all_jpg_files = glob.glob("images/*.jpg")
-#print(all_jpg_files)
-
 
 #extract useful information from each jpg file
 import os
 import shutil
+
 for jpg_file in all_jpg_files:
     file_path = jpg_file
     file_name = os.path.basename(file_path)
@@ -19,9 +17,23 @@ for jpg_file in all_jpg_files:
         "image_path": file_path,
         "image_path_docs": "docs/" + file_path,
         "title": name_only,
-        "output": "docs/" + name_only + ".html",
+        "output_path": "docs/" + name_only + ".html",
         "output_filename": name_only + ".html",
     })
     #copy file over to the /docs/images/ path
     shutil.copy(file_path, "docs/images/" + file_name)
-    print('Successfully processed:', file_path)
+    print('pages list item added for:', file_path)
+
+#using jinja templating, update values per image page and save them into an html file withing /docs/ 
+print(pages)
+from jinja2 import Template
+for page in pages:
+    template_html = open("templates/image_base.html").read()
+    template = Template(template_html)
+    output = template.render(
+        image_source=page['image_path'],
+        title=page['title'],
+        image_text= 'image text will go here',
+    )
+    open(page['output_path'], 'w+').write(output)
+    print('Successfully processed:', page['image_path'], ' | Saved to:', page['output_path'])
