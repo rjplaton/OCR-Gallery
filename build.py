b8cd58f8454f4fd31af8ea39fe6fc8c92087c3ed
rjplaton <rjplaton@gmail.com>
2019-07-03 07:34:36 -0700

    got the jinja template to work and create boxes from face_locations

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 pages = []
+face_locations =[]
 
 #discover all the jpg files in the /images/ directory and save as a list
 import glob
@@ -11,6 +12,7 @@ import os
 import shutil
 from PIL import Image
 import pytesseract
+import face_recognition
 
 for jpg_file in all_jpg_files:
     file_path = jpg_file
@@ -24,7 +26,10 @@ for jpg_file in all_jpg_files:
         image_text = ("Could not find text in the image")
     else:
 	    image_text = (detected_text)
-    
+
+	#use face recognition to supply coords for faces
+    image = face_recognition.load_image_file(jpg_file)
+    face_location = face_recognition.face_locations(image)
     #append jpg file information to a dict in the pages list
     pages.append({
         "image_path": file_path,
@@ -33,6 +38,7 @@ for jpg_file in all_jpg_files:
         "output_path": "docs/" + name_only + ".html",
         "output_filename": name_only + ".html",
         "image_text": image_text,
+        'face_locations': face_location,
     })
     #copy file over to the /docs/images/ path
     shutil.copy(file_path, "docs/images/" + file_name)
@@ -46,12 +52,15 @@ for page in pages:
     template = Template(template_html)
     output = template.render(
     	pages=pages,
+    	face_locations=page['face_locations'],
         image_source=page['image_path'],
         title=page['title'],
         image_text=page['image_text'],
     )
     open(page['output_path'], 'w+').write(output)
     print('Successfully processed:', page['image_path'], ' | Saved to:', page['output_path'])
+
+print('\n'.join(map(str, pages))) 
 
 
 
---
+++ testing.py
@@ -0,0 +1,34 @@
+
+
+pages = [
+{'image_path': 'images/mantra_rock_dance.jpg', 
+  'image_path_docs': 'docs/images/mantra_rock_dance.jpg',
+  'title': 'mantra_rock_dance', 
+  'output_path': 'docs/mantra_rock_dance.html', 
+  'output_filename': 'mantra_rock_dance.html', 
+  'image_text': 'Could not find text in the image', 
+  'face_locations': []},
+{'image_path': 'images/dewey_defeats_truman.jpg', 'image_path_docs': 'docs/images/dewey_defeats_truman.jpg', 'title': 'dewey_defeats_truman', 'output_path': 'docs/dewey_defeats_truman.html', 'output_filename': 'dewey_defeats_truman.html', 'image_text': 'Sie? eg a we nee ee\n\ncrm ne NO at ner a oysnarne HOME ‘OME\nAiea rront\n\nDEWEY DEFEATS TRUMAN\n\nG.0.P. Sweep Indicated in State; dicated in State; Boyle Leads in City\n|Barly Count |UTS 6.0.P.\n\nGives GOP BACK IN THE\nWHITE HOUSE\n\nsizable Electoral\nMargin Seen\n\n   \n\n \n\n \n\n \n\n  \n  \n\nREPUBLICAN 7 iPeercoeien RECORD CITY,\n\nTIOKET AMEAD for Aveorney VOTE SEEN IN\nLATE TALLIES\n\nOF 1944 VOTE [sts rnen\n\nTown Balloting\nGives Trend\nae', 'face_locations': []}
+,{'image_path': 'images/danny_wynter.jpg', 
+'image_path_docs': 'docs/images/danny_wynter.jpg', 
+'title': 'danny_wynter', 
+'output_path': 'docs/danny_wynter.html', 
+'output_filename': 'danny_wynter.html', 
+'image_text': 'Could not find text in the image', 
+'face_locations': [(170, 491, 491, 170)]}
+,{'image_path': 'images/humpty_dumpty.jpg', 'image_path_docs': 'docs/images/humpty_dumpty.jpg', 'title': 'humpty_dumpty', 'output_path': 'docs/humpty_dumpty.html', 'output_filename': 'humpty_dumpty.html', 'image_text': 'Denslow’s\nHUMPTY DUMPTY\n\n—~Adapted and Illustrated by W.WDenslow', 'face_locations': []}
+,{'image_path': 'images/dr_jekyll_and_mr_hyde.jpg', 'image_path_docs': 'docs/images/dr_jekyll_and_mr_hyde.jpg', 'title': 'dr_jekyll_and_mr_hyde', 'output_path': 'docs/dr_jekyll_and_mr_hyde.html', 'output_filename': 'dr_jekyll_and_mr_hyde.html', 'image_text': 'Could not find text in the image', 'face_locations': [(146, 557, 182, 521)]}
+,{'image_path': 'images/samuel_reshevsky.jpg', 'image_path_docs': 'docs/images/samuel_reshevsky.jpg', 'title': 'samuel_reshevsky', 'output_path': 'docs/samuel_reshevsky.html', 'output_filename': 'samuel_reshevsky.html', 'image_text': 'Could not find text in the image', 'face_locations': [(84, 78, 136, 26), (170, 217, 206, 181), (150, 489, 186, 453), (74, 261, 118, 218), (158, 729, 194, 693), (114, 513, 150, 477), (74, 421, 110, 385), (134, 629, 170, 593), (138, 549, 174, 513), (46, 381, 89, 338), (158, 449, 194, 413), (175, 391, 218, 347)]}
+,{'image_path': 'images/war_stamps.jpg', 'image_path_docs': 'docs/images/war_stamps.jpg', 'title': 'war_stamps', 'output_path': 'docs/war_stamps.html', 'output_filename': 'war_stamps.html', 'image_text': 'ay WAR SAVINGS STAMPS _', 'face_locations': [(96, 254, 171, 179)]}
+,{'image_path': 'images/girls_generation.jpg', 'image_path_docs': 'docs/images/girls_generation.jpg', 'title': 'girls_generation', 'output_path': 'docs/girls_generation.html', 'output_filename': 'girls_generation.html', 'image_text': 'Could not find text in the image', 'face_locations': [(182, 285, 218, 249), (198, 117, 234, 81), (202, 205, 238, 169), (186, 425, 222, 389), (198, 517, 234, 481), (194, 737, 230, 701), (178, 349, 214, 313), (190, 589, 226, 553), (194, 661, 230, 625)]}
+,{'image_path': 'images/world_fair.jpg', 'image_path_docs': 'docs/images/world_fair.jpg', 'title': 'world_fair', 'output_path': 'docs/world_fair.html', 'output_filename': 'world_fair.html', 'image_text': 'o-— Be me 2S\n1833 A CENTURY OF PROGRESS 1933|', 'face_locations': []}
+,{'image_path': 'images/startrek.jpg', 'image_path_docs': 'docs/images/startrek.jpg', 'title': 'startrek', 'output_path': 'docs/startrek.html', 'output_filename': 'startrek.html', 'image_text': '= |\n\n7 Nee\n\nme\n\n    \n\n—— uP\n\n. ee', 'face_locations': [(250, 665, 286, 629), (230, 581, 266, 545), (238, 349, 274, 313), (230, 433, 266, 397), (206, 161, 242, 125)]}
+,{'image_path': 'images/royal_wedding.jpg', 'image_path_docs': 'docs/images/royal_wedding.jpg', 'title': 'royal_wedding', 'output_path': 'docs/royal_wedding.html', 'output_filename': 'royal_wedding.html', 'image_text': 'Could not find text in the image', 'face_locations': [(136, 295, 226, 205), (136, 524, 226, 434)]}
+,{'image_path': 'images/plan9.jpg', 'image_path_docs': 'docs/images/plan9.jpg', 'title': 'plan9', 'output_path': 'docs/plan9.html', 'output_filename': 'plan9.html', 'image_text': "UNSPEAKABLE,\n\nHORRORS FROW\n\nOUTER SPACE\nPARANTE\n\n      \n  \n\n  \n  \n\n, TRE LINING.\n\nBA AND RESURREST\nD'\n\n \n\n \n \n\nBELA LUGOS\\\nVAMPARK\n\nLYLE TALBOT\n\nA. edward Reyes Predeon\n\nProduces and Dieeted by\nEdward D. Wood,", 'face_locations': [(454, 182, 583, 53)]}
+,{'image_path': 'images/grace_hopper.jpg', 'image_path_docs': 'docs/images/grace_hopper.jpg', 'title': 'grace_hopper', 'output_path': 'docs/grace_hopper.html', 'output_filename': 'grace_hopper.html', 'image_text': 'Could not find text in the image', 'face_locations': [(96, 225, 186, 135)]}
+]
+
+for page in pages:
+    for face in page['face_locations']:
+        print(page)
+        print(face[0])
